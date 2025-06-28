6b79882c916e011376e2aafd828bde1ab772fa57
Hantat4 <juanluis.12adr@gmail.com>
2025-06-28 13:53:51 -0700

    Actualización: arreglo de organizacion y agregacion de nuevas news

--- Fuse.js
+++ Fuse.js
@@ -6,17 +6,25 @@ const noticias = [
     title: "Psychology Thesis-type Research",
     description: "A thesis-type research on the effect of film violence",
     link: "news/filmthesis.html",
-    image: "css/img/film.png",
+    image: "css/img/film/film.png",
     class: "Psy"
     },
     {
     title: "Genetically modified babies with CRISPR",
     description: "Scientists create genetically modified babies using CRISPR.",
     link: "news/modif.html",
-    image: "css/img/CSP.png",
+    image: "css/img/crispr/CSP.png",
     class: "Mdf"
-    },
+    }, 
     {
+    title: "DeepFakes: New threat?",
+    description: "DeepFakes are a new technology that allows the creation of realistic fake videos.",
+    link: "news/df.html",
+    image: "css/img/deepfake/df.png",
+    class: "df",
+    tags: ["DeepFakes", "technology", "video", "fake", "threat", "AI"]
+    }
+    ,{
     title: "About GNews",
     description: "What is GNews and why it exists",
     link: "about.html",
